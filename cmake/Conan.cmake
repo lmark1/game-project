b8116dc1fc48a56964ecd8bec2d79f5d5281c495
Lovro Markovic <lovro.markovic@fer.hr>
2020-03-29 15:47:21 +0200

    Add CMake commnets

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -10,20 +10,21 @@ endif()
 
 include(${CMAKE_BINARY_DIR}/conan.cmake)
 
+# Setup a remote repository
 conan_add_remote(NAME bincrafters URL
                  https://api.bintray.com/conan/bincrafters/public-conan)
 
 conan_cmake_run(
   REQUIRES
-  ${CONAN_EXTRA_REQUIRES}
-  catch2/2.11.0
-  docopt.cpp/0.6.2
-  fmt/6.0.0
-  spdlog/1.5.0
+  ${CONAN_EXTRA_REQUIRES} # Optional dependancies, initialized outside of this script
+  catch2/2.11.0 # A modern, C++-native, header-only, test framework for unit-tests
+  docopt.cpp/0.6.2 # A popular command line option processing library
+  fmt/6.0.0 # {fmt} is an open-source formatting library providing a fast and safe alternative to C stdio and C++ iostreams.
+  spdlog/1.5.0 # Speed log - a relatively fast logger
   OPTIONS
-  ${CONAN_EXTRA_OPTIONS}
+  ${CONAN_EXTRA_OPTIONS} # Extra options, initialized outisde of this scribt
   BASIC_SETUP
-  CMAKE_TARGETS # individual targets to link to
+  CMAKE_TARGETS # This allows Conan to generate cmake linker targets
   BUILD
   missing)
 endmacro()
--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -1,15 +1,25 @@
-# Set a default build type if none was specified
+# Set a default build type if none was specified A build type indicates a set of compile-time decisions used while
+# compiling your code. For example, whether or not to optimize or to include debug information. A build type or build
+# mode or configuration is popular in Visual Studio, where C++ projects typically have at least two types: Release and
+# Debug.
+
+# When no build type is provided, I found the compilation string to be: g++ /your/code.cpp For Release build type I
+# found the compilation string to be: g++ -O3 -DNDEBUG /your/code.cpp. Note that assert will be disabled with this build
+# type. For Debug build type, I found the compilation string to be: g++ -g /your/code.cpp
+
+# CMake has build types and follows similar rules. For compiling C++, there are at least 4 build types in CMake:
+# Release, Debug, MinSizeRel and RelWithDebInfo.
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-  message(
-    STATUS "Setting build type to 'RelWithDebInfo' as none was specified.")
+  message(STATUS "Setting build type to 'RelWithDebInfo' as none was specified.")
   set(CMAKE_BUILD_TYPE
       RelWithDebInfo
       CACHE STRING "Choose the type of build." FORCE)
   # Set the possible values of build type for cmake-gui, ccmake
-  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release"
-                                               "MinSizeRel" "RelWithDebInfo")
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
 endif()
 
+# Ccache (or “ccache”) is a compiler cache. It speeds up recompilation by caching previous compilations and detecting
+# when the same compilation is being done again.
 find_program(CCACHE ccache)
 if(CCACHE)
   message("using ccache")
@@ -18,13 +28,14 @@ else()
   message("ccache not found cannot use")
 endif()
 
-# Generate compile_commands.json to make it easier to work with clang based
-# tools
+# Generate compile_commands.json to make it easier to work with clang based tools
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
-option(ENABLE_IPO
-       "Enable Iterprocedural Optimization, aka Link Time Optimization (LTO)"
-       OFF)
+# Interprocedural optimization (IPO) is a collection of compiler techniques used in computer programming to improve
+# performance in programs containing many frequently used functions of small or medium length. IPO differs from other
+# compiler optimization because it analyzes the entire program; other optimizations look at only a single function, or
+# even a single block of code.
+option(ENABLE_IPO "Enable Iterprocedural Optimization, aka Link Time Optimization (LTO)" OFF)
 
 if(ENABLE_IPO)
   include(CheckIPOSupported)
@@ -35,5 +46,3 @@ if(ENABLE_IPO)
     message(SEND_ERROR "IPO is not supported: ${output}")
   endif()
 endif()
-
-
